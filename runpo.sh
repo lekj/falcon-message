7986ef6658b00e223debe28d5e73b68045c35d49
lekj <lekj@qq.com>
2020-03-14 12:20:22 +0800

    fix path

--- runpo.sh
+++ runpo.sh
@@ -5,6 +5,6 @@ script_abs=$(readlink -f "$0")
 script_dir=$(dirname $script_abs)
 
 echo "Start RUN:" $(date "+%y-%m-%d-%-H:%-M:%-S") "------" >> "$script_dir/po.log"
-/usr/local/python-2.7/bin/python2.7 "$script_dir/po.py" >> "$script_dir/po.log" 2>&1
+/usr/bin/python2.7 "$script_dir/po.py" >> "$script_dir/po.log" 2>&1
 echo "END   RUN:" $(date "+%y-%m-%d-%-H:%-M:%-S") "------" >> "$script_dir/po.log"
 
