9f90bf8052f38a579ff6b2b6dcc52f6d0bd7af3d
lekj <lekj@qq.com>
2020-11-25 14:45:01 +0800

    fix

--- po.py
+++ po.py
@@ -181,7 +181,7 @@ def get_graph_history( start_time, end_time ):
     	    "content_wx": text_wx
         } 
         ret  = requests.post( url, data=data, timeout=30 )
-        print "im back:" ret.text
+        print "im back:", ret.text
     
     return "Done"
 
@@ -204,7 +204,7 @@ def countit( in_value ):
         lab_rang = "{:.2f}".format( vmax )
         lab_rang_wx = "{:.1f}".format( vmax )
     elif vmax-vmin < 1: 
-        lab_rang = "+{:.2f}".format( vmax )
+        lab_rang = "{:.2f}~{:.2f}｜{:.2f}".format( vmin, vmax, vavg )
         lab_rang_wx = "+{:.1f}".format( vmax )
     else:
         lab_rang = "{:.2f}~{:.2f}｜{:.2f}".format( vmin, vmax, vavg )
